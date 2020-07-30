26247f980cce88579c7cbdb9f8ecbd1875409ebb
이원회 <doggodi303@naver.com>
2020-07-30 17:07:03 +0900

    0730

--- 2.if문/practice_2.py
+++ 2.if문/practice_2.py
@@ -8,4 +8,55 @@
 #     print('<')
 # else :
 #     print('==')
-print('\\안녕하세요')
+
+# 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+# a = int(input())
+# if a >= 90 : 
+#     print('A')
+# elif a>=80:
+#     print('B')
+# elif a>= 70:
+#     print('C')
+# elif a>= 60:
+#     print('D')
+# else:
+#     print('F')
+
+# 윤년
+
+
+# a = int(input())
+# if a % 4 == 0 and a % 100 != 0:
+#     print('1')
+# elif a % 400 == 0:
+#     print('1')
+# else :
+#     print('0')
+
+#좌표
+
+# a = int(input())
+# b = int(input())
+# if a > 0 and b > 0:
+#     print('1')
+# elif a > 0 and b < 0:
+#     print('4')
+# elif a < 0 and b > 0:
+#     print('2')
+# else:
+#     print('3')
+
+#알람 시계
+
+a ,b = map(int,input().split())
+
+if b - 45 < 0 and a == 0:
+    a = 23
+    b = 15 + b 
+    print(a,b ,end=' ')
+else:
+    a = a - 1
+    b = 15 +b
+    print(a,b ,end=' ')
+
+    
